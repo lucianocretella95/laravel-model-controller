ed7f1c1a31e365289b89ee012522abc76b75bfd5
lucianocretella95 <luciano.cretella@tim.it>
2024-07-03 10:58:30 +0200

    stampo film in home.blade

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $dati = config("data");
         $movie = Movie::all();
-        return view('home', $dati);
+		$data = [
+			"result" => $movie,
+			"altrodato" => 42
+		]; 
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da "config/data.php": {{ $home }}</h1>
+{{-- <h1>Dati da "config/data.php": {{ $home }}</h1>
 <p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+<button class="btn btn-primary">Cliccami</button> --}}
+@foreach ($result as $movie)
+    <h3>{{$movie->title}}</h3>
+	<p>{{$movie->nationality}}</p>
+	<span>vote: {{$movie->vote}}</span>
+@endforeach
 @endsection
